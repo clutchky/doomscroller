6aee21104337c5d1fd1826863cbe9323e0c0e039
clutchky <clutchky@gmail.com>
2025-11-12 09:37:34 +0800

    added markdown

--- doomscroller.py
+++ doomscroller.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from os import scandir
+import markdown
 
 app = Flask(__name__)
 
@@ -9,7 +10,12 @@ feed_list = []
 for i in md_files:
     with open(i.path, encoding="utf-8") as f:
         read_data = f.read()
-        feed_list.append({ "name": i.name, "path": i.path, "content": read_data})
+        html_content = markdown.markdown(read_data, extensions=['fenced_code','codehilite'])
+        feed_list.append({
+            "name": i.name, 
+            "path": i.path, 
+            "content": read_data,
+            "html_content": html_content})
 
 @app.route("/")
 def hello_world():

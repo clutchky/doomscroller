48df92dab12c56717c6d218afb078ae6236a4471
clutchky <clutchky@gmail.com>
2025-11-13 02:11:45 +0800

    added random shuffle; enabled tables

--- doomscroller.py
+++ doomscroller.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template
 from os import scandir
 import markdown
+import random
 
 app = Flask(__name__)
 
-md_files = scandir("D:/PKM/0_inbox")
+md_files = list(scandir("D:/PKM/0_inbox"))
+random.shuffle(md_files)
 feed_list = []
 
 for i in md_files:
     with open(i.path, encoding="utf-8") as f:
         read_data = f.read()
-        html_content = markdown.markdown(read_data, extensions=['fenced_code','codehilite'])
+        html_content = markdown.markdown(read_data, extensions=['fenced_code','codehilite', 'tables'])
         feed_list.append({
             "name": i.name, 
             "path": i.path, 
